bbf0aefa5aa1a91918b11f21d4abcc951c76dfa4
arkadiibasovych <arkadiybasovich@gmai.com>
2020-11-07 20:08:58 +0200

    добавляет решение 2,3 задач

---
+++ JS/dz-3.js
@@ -0,0 +1,12 @@
+// Write code under this line
+const getUsersWithGender = (array, gender) => array.filter(array => array.gender === gender).map(({name}) => name);
+
+
+console.log(getUsersWithGender(users, 'male'));
+
+/* [
+  "Moore Hensley",
+  "Ross Vazquez",  
+  "Carey Barr",  
+  "Blackburn Dotson"  
+] */
