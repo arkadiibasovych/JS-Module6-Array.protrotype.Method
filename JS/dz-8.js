d4427a8e376cabaf5801397386c7f93c8cf3e6e2
arkadiibasovych <arkadiybasovich@gmai.com>
2020-11-07 23:09:11 +0200

    добавляет решение 7-й задачи

---
+++ JS/dz-8.js
@@ -0,0 +1,8 @@
+// Write code under this line
+const getUsersWithFriend = (array, friendName) => array;
+
+// console.log(getUsersWithFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+
+// console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]
